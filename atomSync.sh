dcafda11c2b0494b8f22bcc08c8ea00b5ffd6e77
Ryo Aoyama <r.fe51028.r@gmail.com>
2015-07-11 20:03:27 +0900

    Update atomSync.sh

--- atomSync.sh
+++ atomSync.sh
@@ -1,10 +1,25 @@
-ATOMDIR=$HOME/dotfiles/atom
-TARGET=$HOME
+DIR=$HOME/dotfiles
+ATOMDIR=$HOME/dotfiles/atom/.atom
+TARGET=$HOME/.atom
 
-atomfile=.atom
-ln -sf $ATOMDIR/$atomfile/ $TARGET/
-echo "Created $atomfile/"
+if [[ ! -e $TARGET ]] ; then
+    mkdir $TARGET
+    echo "Created $TARGET/"
+fi
+
+cd $ATOMDIR
+for file in * ; do
+  [ $file = "." ] && continue
+  [ $file = ".." ] && continue
+  [ $file = ".DS_Store" ] && continue
+
+  if [[ -f $file ]] ; then
+    ln -sf $ATOMDIR/$file $TARGET/
+    echo "Created /.atom/$file@"
+  fi
+done
+cd $DIR
 
 apm star --installed
 apm stars --install
-echo "Installed starred package"
+echo "Installed starred Atom packages."
