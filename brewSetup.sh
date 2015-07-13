f97e73dbce8bbf13835b69cedd4a0f865e10d04c
Ryo Aoyama <r.fe51028.r@gmail.com>
2015-07-13 14:46:32 +0900

    WIP brewSetup.sh

--- brewSetup.sh
+++ brewSetup.sh
@@ -1,18 +1,26 @@
 
+# TODO: WIPWIPWIPWIPWIPWIP
+
 function brewInstall () {
+  echo "Start brew setup"
   brew tap Homebrew/bundle
   brew bundle
   brew cask cleanup
+  echo "Done"
 }
 
-brew=`which brew 2>&1`
-if [[ $? != 0 ]]; then
-    $((ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)") 2>/dev/null)
-    if [[ $? != 0 ]] ; then
-        brewInstall ;
-    else
-       echo "Install Homebrew is failed." ;
-    fi
-else
-    brewInstall ;
-fi
+brewInstall ;
+
+# brew=`which brew 2>&1`
+# if [[ $? != 0 ]]; then
+#     echo "Installing Homebrew..."
+#     $((ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)") 2>/dev/null)
+#     echo "Done"
+#     if [[ $? != 0 ]] ; then
+#         brewInstall ;
+#     else
+#        echo "Install Homebrew is failed." ;
+#     fi
+# else
+#     brewInstall ;
+# fi
